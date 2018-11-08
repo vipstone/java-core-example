67001975bfe64de3338164268bcf59b9a4c04e0b
王磊 <295628068@qq.com>
2018-11-08 15:54:25 +0800

    add socket example

--- project/src/main/java/thread/IOExample.java
+++ project/src/main/java/thread/IOExample.java
@@ -1,31 +1,49 @@
 package thread;
 
+
+import javafx.scene.input.DataFormat;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class IOExample {
     public static void main(String[] args) throws IOException {
 
-        String filePath = "d:\\log.txt";
-        String Content = "你好，world~\n";
+        String filePath = "D:\\log.txt";
+        String Content = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " surprise \n";
 
         // 添加文件
-        try (FileWriter fileWriter = new FileWriter(filePath, true)) {
-            fileWriter.write(Content);
-            fileWriter.close();
-        }
+        FileWriter fileWriter = new FileWriter(filePath, true);
+        fileWriter.write(Content);
+        fileWriter.close();
 
         // 读取文件
-        // try-with-resources 模式
-        try (FileReader fileReader = new FileReader(filePath);
-             BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-            StringBuffer bf = new StringBuffer();
-            String str;
-            while ((str = bufferedReader.readLine()) != null) {
-                bf.append(str + "\n");
-            }
-            System.out.println(bf.toString());
+        FileReader fileReader = new FileReader(filePath);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        StringBuffer bf = new StringBuffer();
+        String str;
+        while ((str = bufferedReader.readLine()) != null) {
+            bf.append(str + "\n");
         }
+        bufferedReader.close();
+        fileReader.close();
+        System.out.println(bf.toString());
+
+        // ====================JDK 1.7 之后 读写文件====================
+        // 写入文件（追加方式：StandardOpenOption.APPEND）
+        Files.write(Paths.get(filePath), Content.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+
+        // 读取文件
+        byte[] data =  Files.readAllBytes(Paths.get(filePath));
+        System.out.println(new String(data, StandardCharsets.UTF_8));
+
+        // 创建多（单）层目录（如果不存在创建，存在不会报错）
+        Files.createDirectories(Paths.get("D://a//b"));
+
+        // JDK 1.7 之前 创建多（单）层目录（如果不存在创建，存在不会报错）
+        new File("D://a//b").mkdirs();
 
     }
 }
---
+++ project/src/main/java/thread/SocketExample.java
@@ -0,0 +1,160 @@
+package thread;
+
+import javax.xml.ws.handler.Handler;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.*;
+import java.util.logging.SimpleFormatter;
+
+public class SocketExample {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        int port = 4343; //端口号
+//        // Socket 服务器端（简单的发送信息）
+//        Thread sThread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    ServerSocket serverSocket = new ServerSocket(port);
+//                    while (true) {
+//                        // 等待连接
+//                        Socket socket = serverSocket.accept();
+//                        Thread sHandlerThread = new Thread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                try (PrintWriter printWriter = new PrintWriter(socket.getOutputStream())) {
+//                                    printWriter.println("hello world！");
+//                                    printWriter.flush();
+//                                } catch (IOException e) {
+//                                    e.printStackTrace();
+//                                }
+//                            }
+//                        });
+//                        sHandlerThread.start();
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//        sThread.start();
+//
+//        // Socket 客户端（接收信息并打印）
+//        try (Socket cSocket = new Socket(InetAddress.getLocalHost(), port)) {
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
+//            bufferedReader.lines().forEach(s -> System.out.println("客户端：" + s));
+//        } catch (UnknownHostException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        // NIO线程复用版
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(4, 4,
+                60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+        threadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                try (Selector selector = Selector.open();
+                     ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();) {
+                    serverSocketChannel.bind(new InetSocketAddress(InetAddress.getLocalHost(), port));
+                    serverSocketChannel.configureBlocking(false);
+                    serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+                    while (true) {
+                        selector.select(); // 阻塞等待就绪的Channel
+                        Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                        Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                        while (iterator.hasNext()) {
+                            SelectionKey key = iterator.next();
+                            try (SocketChannel channel = ((ServerSocketChannel) key.channel()).accept()) {
+                                channel.write(Charset.defaultCharset().encode("你好，世界"));
+                            }
+                            iterator.remove();
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        // Socket 客户端（接收信息并打印）
+        try (Socket cSocket = new Socket(InetAddress.getLocalHost(), port)) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(cSocket.getInputStream()));
+            bufferedReader.lines().forEach(s -> System.out.println("NIO 客户端：" + s));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        // AIO线程复用版
+//        Thread sThread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                AsynchronousChannelGroup group = null;
+//                try {
+//                    group = AsynchronousChannelGroup.withThreadPool(Executors.newFixedThreadPool(4));
+//                    AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open(group).bind(new InetSocketAddress(InetAddress.getLocalHost(), port));
+//                    server.accept(null, new CompletionHandler<AsynchronousSocketChannel, AsynchronousServerSocketChannel>() {
+//                        @Override
+//                        public void completed(AsynchronousSocketChannel result, AsynchronousServerSocketChannel attachment) {
+//                            server.accept(null, this); // 接收下一个请求
+//                            try {
+//                                Future<Integer> f = result.write(Charset.defaultCharset().encode("你好，世界"));
+//                                f.get();
+//                                System.out.println("服务端发送时间：" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+//                                result.close();
+//                            } catch (InterruptedException | ExecutionException | IOException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//
+//                        @Override
+//                        public void failed(Throwable exc, AsynchronousServerSocketChannel attachment) {
+//                        }
+//                    });
+//                    group.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
+//                } catch (IOException | InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//        sThread.start();
+//
+//        // Socket 客户端
+//        AsynchronousSocketChannel client = AsynchronousSocketChannel.open();
+//        Future<Void> future = client.connect(new InetSocketAddress(InetAddress.getLocalHost(), port));
+//        future.get();
+//        ByteBuffer buffer = ByteBuffer.allocate(100);
+//        client.read(buffer, null, new CompletionHandler<Integer, Void>() {
+//            @Override
+//            public void completed(Integer result, Void attachment) {
+//                System.out.println("客户端打印：" + new String(buffer.array()));
+//            }
+//
+//            @Override
+//            public void failed(Throwable exc, Void attachment) {
+//                exc.printStackTrace();
+//                try {
+//                    client.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//        Thread.sleep(10 * 1000);
+    }
+}
+
+
+
